66db349eb6005a1ad2f33afcc50c485d309d770c
madhumithaa <madhumithaa1411@gmail.com>
2020-09-09 10:18:40 +0530

    Defined utilitlity pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Login, Dashboard } from "./pages";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { Login, Dashboard, Page404 } from "./pages";
 import { AuthenticatedRoute, NonAuthenticatedRoute } from "./helpers/routes";
 export default class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
+          <Redirect exact from="/" to="/login" />
           <NonAuthenticatedRoute path="/login" component={Login} />
           <AuthenticatedRoute path="/dashboard" component={Dashboard} />
+          <Route component={Page404} />
         </Switch>
       </Router>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Login } from "./Login";
 export { default as Dashboard } from "./Dashboard";
+export { default as Page404 } from "./Page404";
