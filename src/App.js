9ef17c4a88fedbcc10d04ed43faa98f9d466e6e8
madhumithaa <madhumithaa1411@gmail.com>
2020-09-09 09:59:22 +0530

    routes init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Login, Dashboard } from "./pages";
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
-
-export default App;
