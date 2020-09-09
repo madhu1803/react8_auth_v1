24da327e4ec9516d4116b38a7d069d29582628db
madhumithaa <madhumithaa1411@gmail.com>
2020-09-09 11:12:27 +0530

    header right done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,14 +3,20 @@ import React, { Component } from "react";
 export default class Login extends Component {
   render() {
     return (
-      <div className="login-container">
+      <div className="login-container text-capitalize">
         <div className="left-container">
-          <div>
-            <img src="https://i.imgur.com/o3FvBnt.png" />
-            <h3>India's First Customized InStore Solutions</h3>
+          <img src="https://i.imgur.com/o3FvBnt.png" />
+          <h3>India's First Customized InStore Solutions</h3>
+        </div>
+        <div className="right-container">
+          <div className="right-header">
+            <h4>
+              <strong>Retail Hub</strong>
+            </h4>
+            <small className="text-muted">Retailscan</small>
+            <p className="pt-5 text-muted">Please Login to your account</p>
           </div>
         </div>
-        <div className="right-container"></div>
       </div>
     );
   }
